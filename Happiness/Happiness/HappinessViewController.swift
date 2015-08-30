56f65f90c7beb5efb02d4144a9db18e4784c12df
GSerjo <sedot8@gmail.com>
2015-08-30 12:37:46 +0300

    * Added @IBDesignable

--- Happiness/Happiness/HappinessViewController.swift
+++ Happiness/Happiness/HappinessViewController.swift
@@ -8,7 +8,19 @@
 
 import UIKit
 
-class HappinessViewController: UIViewController {
+final class HappinessViewController: UIViewController {
 
+    // 0 = very sad, 100 = esctatic
+    var happiness: Int = 50 {
+        didSet {
+            happiness = min(max(happiness, 0), 100)
+            print("happiness = \(happiness)")
+            updateUI()
+        }
+    }
+    
+    func updateUI() {
+        
+    }
 
 }
