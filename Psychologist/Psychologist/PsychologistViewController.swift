1d354917ecd49eebc3481b34a072a87ca38403ea
GSerjo <sedot8@gmail.com>
2015-09-01 19:29:38 +0300

    + PsychologistViewController initial version

---
+++ Psychologist/Psychologist/PsychologistViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Psychologist
+//
+//  Created by Serjo on 01/09/15.
+//  Copyright © 2015 Serjo. All rights reserved.
+//
+
+import UIKit
+
+class PsychologistViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
