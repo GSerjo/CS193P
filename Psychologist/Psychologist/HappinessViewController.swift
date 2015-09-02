42a59477c9c93236b10175b4cec1b0c1d4501bb4
GSerjo <sedot8@gmail.com>
2015-09-02 23:58:46 +0300

    + HappinessViewController

---
+++ Psychologist/Psychologist/HappinessViewController.swift
@@ -0,0 +1,55 @@
+//
+//  HappinessViewController.swift
+//  Happiness
+//
+//  Created by Serjo on 29/08/15.
+//  Copyright © 2015 Serjo. All rights reserved.
+//
+
+import UIKit
+
+final class HappinessViewController: UIViewController, FaceViewDataSource {
+    
+    private struct Constants {
+        static let HappinessGestureScale: CGFloat = 4
+    }
+    
+    @IBOutlet weak var faceView: FaceView! {
+        didSet {
+            faceView.dataSource = self
+            faceView.addGestureRecognizer(UIPinchGestureRecognizer(target: faceView, action: "scale:"))
+        }
+    }
+
+    @IBAction func changeHappiness(gesture: UIPanGestureRecognizer) {
+        switch gesture.state {
+        case .Ended: fallthrough
+        case .Changed:
+            let translation = gesture.translationInView(faceView)
+            let happinessChange = -Int(translation.y / Constants.HappinessGestureScale)
+            if happinessChange != 0 {
+                happiness += happinessChange
+                gesture.setTranslation(CGPointZero, inView: faceView)
+            }
+        default: break
+        }
+    }
+    
+    // 0 = very sad, 100 = esctatic
+    var happiness: Int = 100 {
+        didSet {
+            happiness = min(max(happiness, 0), 100)
+            print("happiness = \(happiness)")
+            updateUI()
+        }
+    }
+    
+    func updateUI() {
+        faceView?.setNeedsDisplay()
+    }
+    
+    func smilinessForFaceView(sender: FaceView) -> Double? {
+        return Double(happiness - 50)/50
+    }
+
+}
