ec21d369e1c23af7384e2cc4bcc99972a14f14ca
GSerjo <sedot8@gmail.com>
2015-11-10 00:12:07 -0500

    * added load tweeter's image

--- Smashtag/Smashtag/ImageTableViewCell.swift
+++ Smashtag/Smashtag/ImageTableViewCell.swift
@@ -10,13 +10,41 @@ import UIKit
 
 final class ImageTableViewCell: UITableViewCell {
     
-    var imageUrl: NSURL?
+    var imageUrl: NSURL? {
+        didSet {
+            updateUI()
+        }
+    }
 
+    @IBOutlet weak var tweetImage: UIImageView!
+    @IBOutlet weak var spinner: UIActivityIndicatorView!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
     }
 
     override func setSelected(selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
+    }
+    
+    private func updateUI() {
+        if let url = imageUrl {
+            spinner?.startAnimating()
+            let qos = Int(QOS_CLASS_USER_INITIATED.rawValue)
+            dispatch_async(dispatch_get_global_queue(qos, 0)) {
+                ()-> Void in let imageData = NSData(contentsOfURL: url)
+                dispatch_async(dispatch_get_main_queue()) {
+                    if url == self.imageUrl {
+                        if imageData != nil {
+                            self.tweetImage?.image = UIImage(data: imageData!)
+                        }
+                        else {
+                            self.tweetImage?.image = nil
+                        }
+                        self.spinner?.stopAnimating()
+                    }
+                }
+            }
+        }
     }
 }
